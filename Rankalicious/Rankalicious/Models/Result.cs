1eaf25d30bf93c27b917e578522dd31d15d5c5cd
Will Cohen <whc1989@hotmail.com>
2014-10-02 23:30:44 +1000

    Initial project CheckIn

---
+++ Rankalicious/Rankalicious/Models/Result.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rankalicious.Models
+{
+    public class Result
+    {
+    }
+}
---
+++ Rankalicious/Rankalicious/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rankalicious.Startup))]
+namespace Rankalicious
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
